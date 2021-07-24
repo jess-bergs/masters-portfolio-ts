37ff56c5b32e7dc268c9030c2081fca47d771b1f
jess-bergs <jessica.bergs@bbc.co.uk>
2021-07-24 18:33:28 +0100

    Test: add missing test for Video component

--- test/components/Video.test.tsx
+++ test/components/Video.test.tsx
@@ -4,24 +4,31 @@ import '@testing-library/jest-dom';
 import Video from '../../src/components/Video/Video';
 
 const videoContent = {
-    url: 'a YouTube url',
+    url: 'a video url',
     posterImageUrl: 'a poster image url',
 };
 
-const renderComponent = () => {
-    return render(<Video {...videoContent} />);
+const renderComponent = (customProps?) => {
+    return render(<Video {...(customProps || videoContent)} />);
 };
 
 describe('the Video component', () => {
     it('sets the url as the video source', () => {
         const { getByTitle } = renderComponent();
-        expect(getByTitle('video-source')).toHaveAttribute('src', 'a YouTube url');
+        expect(getByTitle('video-source')).toHaveAttribute('src', 'a video url');
     });
 
     describe('when a poster image is passed', () => {
         it('sets the image as the posterimage', () => {
             const { getByTitle } = renderComponent();
             expect(getByTitle('video')).toHaveAttribute('poster', 'a poster image url');
+        });
+    });
+
+    describe('when no poster image is passed', () => {
+        it('no poster is rendered', () => {
+            const { queryByTitle } = renderComponent({ url: 'a video url' });
+            expect(queryByTitle('video')).not.toHaveAttribute('poster');
         });
     });
 });
