cccd06d8e5132309d706a0ddbfacee99fd807628
jess-bergs <jessica.bergs@bbc.co.uk>
2021-07-29 15:07:36 +0100

    Test: fix failing Homepage tests by adding  Router wrapping to Homepage render

--- test/pages/Homepage.test.tsx
+++ test/pages/Homepage.test.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Homepage from '../../src/pages/Homepage/Homepage';
 
 const projects = [
-    { title: 'title1', subline: 'subline1', previewImageUrl: 'url1' },
-    { title: 'title2', subline: 'subline2', previewImageUrl: 'url2' },
-    { title: 'title3', subline: 'subline3', previewImageUrl: 'url3' },
+    { title: 'title1', subline: 'subline1', previewImageUrl: 'url1', pagePath: '/' },
+    { title: 'title2', subline: 'subline2', previewImageUrl: 'url2', pagePath: '/' },
+    { title: 'title3', subline: 'subline3', previewImageUrl: 'url3', pagePath: '/' },
 ];
+
 const renderComponent = () => {
-    return render(<Homepage projects={projects} />);
+    return render(
+        <Router>
+            <Homepage projects={projects} />
+        </Router>,
+    );
 };
 
-describe('the Project page', () => {
+describe('the Homepage', () => {
     it('renders the passed project title', () => {
         const { getAllByText } = renderComponent();
         expect(getAllByText(/title.*/)).toHaveLength(3);
