ff69a9aac77f8a9a4bef07898bc4ac0352262b58
jess-bergs <jessica.bergs@bbc.co.uk>
2021-07-31 15:16:50 +0100

    Chore: add custom viewports to storybook to reflect app's breakpoints

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,50 @@
 
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
+const customViewports = {
+    mobilePortrait: {
+        name: 'mobile',
+        styles: {
+            width: '360px',
+            height: '700px',
+        },
+    },
+    tabletPortrait: {
+        name: 'tablet',
+        styles: {
+            width: '768px',
+            height: '1024px',
+        },
+    },
+    desktopSmall: {
+        name: 'desktop – WXGA',
+        styles: {
+            width: '1440px',
+            height: '1080px',
+        },
+    },
+
+    desktop: {
+        name: 'desktop – FHD',
+        styles: {
+            width: '1920px',
+            height: '1080px',
+        },
+    },
+};
+
+
 export const parameters = {
     backgrounds: {
         default: 'neutral',
         values: [
             { name: 'neutral', value: '#aaa' },
             { name: 'Live', value: '#000' },
-            { name: 'contrast', value: '#ff00aa' },
+            { name: 'contrast', value: '#f0a' },
         ],
     },
     viewport: {
-        viewports: INITIAL_VIEWPORTS
+        viewports: { ...customViewports, ...INITIAL_VIEWPORTS }
     },
     layout: 'fullscreen'
 }
