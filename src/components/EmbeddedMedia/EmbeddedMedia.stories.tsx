4f774bc1ad1725599682145df059e94495239633
jess-bergs <jessica.bergs@bbc.co.uk>
2021-08-01 23:21:19 +0100

    Task: add poster image to EmbeddedMedia component to improve display while loading

--- src/components/EmbeddedMedia/EmbeddedMedia.stories.tsx
+++ src/components/EmbeddedMedia/EmbeddedMedia.stories.tsx
@@ -7,8 +7,16 @@ export default {
     title: 'Components/EmbeddedMedia',
 };
 
-export const YouTube = () => <EmbdeddedMedia url="https://www.youtube.com/embed/5h5-iIPu7bM" />;
-
 export const Sketchfab = () => (
-    <EmbdeddedMedia url="https://sketchfab.com/models/d5ba8a80db4e4c9d9925fbbab52a03c5/embed" />
+    <EmbdeddedMedia
+        url="https://sketchfab.com/models/d5ba8a80db4e4c9d9925fbbab52a03c5/embed"
+        posterImageSrc="https://masters-portfolio-assets.s3.eu-central-1.amazonaws.com/images/goma/embedded-media-posters/iframe-poster-sketchfab-plate.jpg"
+    />
+);
+
+export const YouTube = () => (
+    <EmbdeddedMedia
+        url="https://www.youtube.com/embed/5h5-iIPu7bM"
+        posterImageSrc="https://masters-portfolio-assets.s3.eu-central-1.amazonaws.com/images/goma/embedded-media-posters/iframe-poster-unity-yt-video.jpg"
+    />
 );
