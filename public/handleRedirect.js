111ea816167b231d61d49445c8066ccded428b45
jess-bergs <jessica.bergs@bbc.co.uk>
2021-08-06 00:45:39 +0100

    Refactor: extract SPA redirection script in index.html into file

--- public/handleRedirect.js
+++ public/handleRedirect.js
@@ -1,11 +1,18 @@
-
-((l) => {
+/* eslint-disable func-names */
+(function (l) {
     if (l.search[1] === '/') {
-        const decoded = l.search.slice(1).split('&').map((s) => {
-            return s.replace(/~and~/g, '&')
-        }).join('?');
-        window.history.replaceState(null, null,
-            l.pathname.slice(0, -1) + decoded + l.hash
+        const decoded = l.search
+            .slice(1)
+            .split('&')
+            // eslint-disable-next-line func-names
+            .map(function (s) {
+                return s.replace(/~and~/g, '&');
+            })
+            .join('?');
+        window.history.replaceState(
+            null,
+            null,
+            l.pathname.slice(0, -1) + decoded + l.hash,
         );
     }
-})(window.location)
+})(window.location);
